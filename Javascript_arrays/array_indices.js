e9f11c9895037215340347070092e677759f3567
clementdevv <clementdevv@gmail.com>
2026-02-15 21:24:01 +0300

    array indexing and access

---
+++ Javascript_arrays/array_indices.js
@@ -0,0 +1,11 @@
+// Elements in an array can be accessed using their indices (values used to identify their positions)
+
+let days = ['Monday', 'Saturday', 'Wednesday'] 
+
+console.log(days[2])
+
+days[1] = "Sunday"
+console.log(days[1]) // becomes Sunday instead of Saturday since we have changed the element at that position
+
+days[6] = 77
+console.log(days)
