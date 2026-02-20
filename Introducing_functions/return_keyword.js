2aacee35b5cd3adbc30cf099994da11a1194c9b1
clementdevv <clementdevv@gmail.com>
2026-02-20 15:27:29 +0300

    Section 21: lexical scope

--- Introducing_functions/return_keyword.js
+++ Introducing_functions/return_keyword.js
@@ -11,6 +11,57 @@ function isAuthenticated(token) {
     }
 }
 
-isAuthenticated()
+console.log(isAuthenticated(true))
 
-isAuthenticated(true)
+
+function multiply(a, b) {
+    return (a*b);
+}
+
+console.log(multiply(2,3))
+
+function isShortsWeather(temp) {
+    if(temp >=75) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isShortsWeather(80))
+
+function capitalize(word) {
+    const firstLetterCaps = word[0].toUpperCase();
+    const restOfString = word.slice(1);
+    return firstLetterCaps + restOfString;    
+}
+
+console.log(capitalize('be cautius lest you pee on your pants'))
+
+
+function sumArray(nums) {
+     let total = 0;
+     for (let num of nums) {
+         total += num;
+     }
+     return total;
+}
+
+console.log(sumArray([1,2,3,4]))
+
+
+
+
+function returnDay(num) {
+    
+ const daysOfTheWeek = ['Monday','Tuesday', 'Wednesday', 'Thursday','Friday', 'Saturday','Sunday']
+    
+    if (num < 1 || num > 7) {
+    return null;
+    } else {
+    return daysOfTheWeek[num - 1];
+}
+
+}
+
+console.log(returnDay(5))
